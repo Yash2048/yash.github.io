ef7c4873b29afaf7a90c243aedf3c0e3927dc96c
Yash <yashraj.ch2048@gmail.com>
2023-09-25 11:28:41 +0530

    added event listener

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("bu");
+button.addEventListener("click", ()=>{
+    document.body.style.backgroundColor = "#393A38";
+    button.style.backgroundColor = " #f5d2a5";
+    button.style.color= "black"
+})
