8e75c8b0dedb023a23135108bd6ed3568e3d1b3f
Yash <yashraj.ch2048@gmail.com>
2023-09-25 16:17:50 +0530

    temp changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
-let button = document.getElementById("bu");
+let mode = false;
+const button = document.getElementById("bu");
 button.addEventListener("click", ()=>{
-    document.body.style.backgroundColor = "#393A38";
-    button.style.backgroundColor = " #f5d2a5";
-    button.style.color= "black"
+    if(mode==false){
+        document.body.style.backgroundColor = "#393A38";
+        button.style.backgroundColor = " #f5d2a5";
+        button.style.color= "black"
+        mode = true;
+    }
+    else{
+        document.body.style.backgroundColor = "white";
+        button.style.backgroundColor = " #9e8787";
+        button.style.color= "#35060f"
+        mode = false;
+
+    }
 })
